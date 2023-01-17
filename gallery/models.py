ec62aa75718dbbe8b82df2d639a357809fac5640
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2023-01-17 11:50:58 +0600

    campaign id resolved in gallery, null data terminated

--- gallery/models.py
+++ gallery/models.py
@@ -19,7 +19,7 @@ class Gallery(models.Model):
     slug = models.SlugField(max_length=500, null=True, unique=True)
     image = models.ImageField(upload_to=generate_filename, null=True)
     # project = models.ForeignKey(Projects, default=None, on_delete=models.CASCADE, related_name="gallery")
-    campaign = models.ForeignKey(Campaigns, default=None,null=True, on_delete=models.CASCADE, related_name="gallery")
+    campaign = models.ForeignKey(Campaigns, default=None, on_delete=models.CASCADE, related_name="gallery")
 
 
     
